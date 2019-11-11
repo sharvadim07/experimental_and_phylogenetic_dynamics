70a7253a736ac622ae46d13acabc27f954d8da4d
--global <--global>
2019-11-11 13:33:31 +0700

    Generate equation.c and ParamRanges.txt files.

--- expdyn_gen_debinfer_in.py
+++ expdyn_gen_debinfer_in.py
@@ -26,9 +26,9 @@ def read_equations_file(equations_file_name):
             if line[:2] == 'dx':
                 eq_counter += 1
                 eq_name_reres = re.search(r'dx\[\,\'(.+)\'\]=', line)
-                eq_name = eq_name_reres.group(1)
-                repl_dict[eq_name_reres.group(1)] = 'x[' + str(eq_counter) + ']'
+                eq_name = eq_name_reres.group(1)               
                 equations_dict[eq_name] = []
+                repl_dict[eq_name] = 'x[' + str(eq_counter) + ']'
                 line_str_terms = line.strip().split('+(')[1:]
                 for str_term in line_str_terms:
                     #equations_dict[eq_name].append(re.sub('[\[\]\'p\(\)\,x ]', '', str_term))
@@ -39,10 +39,12 @@ def read_init_values_file(init_values_file_name):
     par_init_values_dict = {}
     var_init_values_dict = {}
     par_name_list = []
-    var_name_list = []
+    #var_name_list = []
     with open(init_values_file_name, 'r') as init_values_file:
         StartValuesOfparameters = False
         StartValuesOfequations = False
+        param_counter = 0
+        #var_counter = 0
         for line in init_values_file:
             if line == '\n':
                 StartValuesOfparameters = False
@@ -55,33 +57,38 @@ def read_init_values_file(init_values_file_name):
                 StartValuesOfparameters = False
                 StartValuesOfequations = True                
                 continue
-            if StartValuesOfparameters == True and StartValuesOfequations == False:                
+            if StartValuesOfparameters == True and StartValuesOfequations == False:                           
                 raw_name, value = line.strip().split('=')
                 par_spec = raw_name[0] + raw_name[2:] 
-                par_name = par_spec.split('_')[0]
-                par_name_list.append(par_name)
+                par_name = par_spec.split('_')[0]               
+                if par_name not in par_name_list:
+                     param_counter += 1   
+                     par_name_list.append(par_name)
+                     repl_dict[par_name] = 'a[' + str(param_counter) + ']'  
                 try:
                     par_init_values_dict[par_spec] = float(value)
                 except ValueError:
                     par_init_values_dict[par_spec] = value
             if StartValuesOfparameters == False and StartValuesOfequations == True:
+                #var_counter += 1
                 raw_name, value = line.strip().split('=')
                 var_spec = raw_name.split('_')[0] + '_' + raw_name.split('_')[2]
-                var_name = var_spec.split('_')[0]
-                var_name_list.append(var_name)
+                #var_name = var_spec.split('_')[0]
+                #var_name_list.append(var_name)
                 try:
                     var_init_values_dict[var_spec] = float(value)
                 except ValueError:
-                    var_init_values_dict[var_spec] = value                                    
+                    var_init_values_dict[var_spec] = value    
+                                            
     # Add missing specs for parameters
     for par_name in par_name_list:
         if par_name + '_min' not in par_init_values_dict:
             #init_values_dict[par_var_name + '_min'] = init_values_dict[par_var_name + '_initval']            
-            par_init_values_dict[par_name + '_min'] = par_init_values_dict[par_var_name + '_initval']/10.0
+            par_init_values_dict[par_name + '_min'] = par_init_values_dict[par_name + '_initval']/10.0
         if par_name + '_max' not in par_init_values_dict:    
             #init_values_dict[par_var_name + '_max'] = init_values_dict[par_var_name + '_initval']
-            par_init_values_dict[par_name + '_max'] = par_init_values_dict[par_var_name + '_initval']*10.0
-    return par_init_values_dict , var_init_values_dict
+            par_init_values_dict[par_name + '_max'] = par_init_values_dict[par_name + '_initval']*10.0
+    return par_init_values_dict, var_init_values_dict, par_name_list
 
 def get_Jacobian(equations_dict):
     import symengine
@@ -95,7 +102,7 @@ def get_Jacobian(equations_dict):
             J[i,j] = symengine.diff(fi, s)
     return J
 
-def edit_equation_C_code(c_code_pattern_file_name, equations_dict, init_values_dict, params_counter):    
+def edit_equation_C_code(c_code_pattern_file_name, equations_dict, params_counter):    
     with open(c_code_pattern_file_name, 'r') as c_code_pattern_file:
         with open('updated_' + os.path.basename(c_code_pattern_file_name), 'w') as updated_c_code_file:
             need_add_num_eq = False
@@ -149,14 +156,18 @@ def edit_equation_C_code(c_code_pattern_file_name, equations_dict, init_values_d
                 else:
                     updated_c_code_file.write(c_code_patt_line) 
 
-def generate_param_ranges_file(param_ranges_file_name, init_values_dict):
+def generate_param_ranges_file(param_ranges_file_name, par_init_values_dict, par_name_list):
     with open(param_ranges_file_name, 'w') as param_ranges_file:
-        pass
-
+        param_ranges_file.write('Param\tMin\tMax\t# Comment\n')
+        for par in par_name_list:
+            param_ranges_file.write(repl_dict[par].replace('[','').replace(']','') + '\t' + \
+                                    str(par_init_values_dict[par + '_min']) + '\t' + \
+                                    str(par_init_values_dict[par + '_max']) + '\t' + \
+                                    '# ' + par + '\n')
 
 equations_dict = read_equations_file(args.equations)
-init_values_dict = read_init_values_file(args.equations)
+par_init_values_dict, var_init_values_dict, par_name_list = read_init_values_file(args.equations)
 
 
-edit_equation_C_code(os.path.dirname(os.path.realpath(__file__)) + "/equation.c", equations_dict, init_values_dict, params_counter)
-generate_param_ranges_file(param_ranges_file_name, init_values_dict)
+edit_equation_C_code(os.path.dirname(os.path.realpath(__file__)) + "/equation.c", equations_dict, len(par_init_values_dict))
+generate_param_ranges_file('ParamRanges.txt', par_init_values_dict, par_name_list)
